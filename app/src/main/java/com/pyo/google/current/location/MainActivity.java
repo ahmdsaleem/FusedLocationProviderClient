66bb162827ea8fe36d0eed8010e971da073464aa
ahmad <ahmdsaleem@hotmail.com>
2018-01-27 13:42:25 +0500

    FusedLocationProviderClient

---
+++ app/src/main/java/com/pyo/google/current/location/MainActivity.java
@@ -0,0 +1,182 @@
+package com.pyo.google.current.location;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private LocationRequest mLocationRequest;
+
+    private LocationCallback mLocationCallback;
+
+    private Location mCurrentLocation;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    private String provider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        provider = getIntent().getStringExtra("provider");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            checkMyPermissionLocation();
+        } else {
+            initGoogleMapLocation();
+        }
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                /*if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }*/
+                mMap = googleMap;
+                mMap.getUiSettings().setZoomControlsEnabled(true);
+                //mMap.setMyLocationEnabled(true);
+            }
+        });
+    }
+
+    private GoogleMap mMap;
+
+    private void checkMyPermissionLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Permission Check
+            PermissionSettingUtils.requestPermission(this);
+        } else {
+            initGoogleMapLocation();
+        }
+    }
+
+    private void initGoogleMapLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        SettingsClient mSettingsClient = LocationServices.getSettingsClient(this);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult result) {
+                super.onLocationResult(result);
+                //mCurrentLocation = locationResult.getLastLocation();
+                mCurrentLocation = result.getLocations().get(0);
+
+                MarkerOptions options = new MarkerOptions();
+                options.position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()));
+                BitmapDescriptor icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN);
+                options.icon(icon);
+                Marker marker = mMap.addMarker(options);
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 17));
+                //mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability availability) {
+                //boolean isLocation = availability.isLocationAvailable();
+            }
+        };
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setNumUpdates(1);
+        if (provider.equalsIgnoreCase(LocationManager.GPS_PROVIDER)) {
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }else{
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        }
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest mLocationSettingsRequest = builder.build();
+
+        Task<LocationSettingsResponse> locationResponse = mSettingsClient.checkLocationSettings(mLocationSettingsRequest);
+        locationResponse.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.e("Response", "Successful acquisition of location information!!");
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            }
+        });
+        locationResponse.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.e("onFailure", "위치환경체크");
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        String errorMessage = "위치설정체크";
+                        Log.e("onFailure", errorMessage);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != PermissionSettingUtils.REQUEST_CODE) {
+            return;
+        }
+        if (PermissionSettingUtils.isPermissionGranted(new String[]{
+                      Manifest.permission.ACCESS_FINE_LOCATION}, grantResults)) {
+            initGoogleMapLocation();
+        } else {
+            Toast.makeText(this, "위치정보사용 허락을 하지않아 앱을 중지합니다", Toast.LENGTH_SHORT).show();
+            //finish();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
+}
